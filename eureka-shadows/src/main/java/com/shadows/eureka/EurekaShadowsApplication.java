99570cbb13113ab57614e21f343d125ffb01a535
hpp <huapei.tux@gmail.com>
2019-08-14 23:48:18 +0800

    eureka注册，高可用，安全认证，ribbon初探

--- eureka-shadows/src/main/java/com/shadows/eureka/EurekaShadowsApplication.java
+++ eureka-shadows/src/main/java/com/shadows/eureka/EurekaShadowsApplication.java
@@ -3,6 +3,9 @@ package com.shadows.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +15,12 @@ public class EurekaShadowsApplication {
         SpringApplication.run(EurekaShadowsApplication.class, args);
     }
 
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
+    }
 }
--- movie/src/main/java/com/shadows/movie/controller/UserController.java
+++ movie/src/main/java/com/shadows/movie/controller/UserController.java
@@ -1,23 +1,36 @@
 package com.shadows.movie.controller;
 
 import com.shadows.movie.bean.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@Slf4j
 public class UserController {
     private final RestTemplate restTemplate;
+    private final LoadBalancerClient loadBalancerClient;
 
-    public UserController(RestTemplate restTemplate) {
-
+    public UserController(RestTemplate restTemplate, LoadBalancerClient loadBalancerClient) {
         this.restTemplate = restTemplate;
+        this.loadBalancerClient = loadBalancerClient;
     }
+
+    private final static String userService = "http://user-service";
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        return this.restTemplate.getForObject("http://localhost:8080/user/" + id, User.class);
+        return this.restTemplate.getForObject(userService+ "/user/" + id, User.class);
+    }
+
+    @GetMapping("/logUserInstance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("user-service");
+        log.info("=====user instance {}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
     }
 
 }
